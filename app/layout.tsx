3a70523b11a67cfb40f2c7d8442095a978fb8635
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-01-12 20:02:00 +0000

    Tweaked the navbars to make them closer to expected design

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,14 +71,21 @@ export function SearchBar() {
   }
 
   return (
-    <div className="flex items-center">  
+    <div className="flex items-center content-center">  
       <div className="relative w-full">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
         </div>
-        <input onChange={handleChange} onKeyDown={handleEnter} type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+        <input onChange={handleChange} onKeyDown={handleEnter} type="text" id="simple-search" 
+        className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  
+        bg-gray-50 border border-gray-300 text-gray-900 
+        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200 dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        placeholder="Search" required />
       </div>
-      <button onClick={handleClick} type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-[#FF6720] rounded-lg border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-[#FF6720] dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+      <button onClick={handleClick} type="submit" 
+      className="p-2.5 ml-2 text-sm font-medium 
+      text-white bg-[#FF6720] rounded-lg border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 
+      dark:bg-[#FF6720] dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         <span className="sr-only">Search</span>
       </button>
@@ -92,18 +99,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
+  //planet {
+  //  display: grid;
+  //  grid-template-columns: 100px 25% 1fr;
+  //  grid-template-rows: 1fr 100px 25%;
+  //}
+
   return (
-    <html className="bg-[#F3EFE8]">
-      
+    <html className="bg-[#F3EFE8]
+    dark:bg-gray-900">
       
       <head />
-      <body>
-        <Image
-          src="/../images/foodbanklogo2.svg"
-          alt="logo-image"
-          width="300"
-          height="200"
-        />
+      <body className='grid [grid-template-rows: 200px 1fr]'>
         <SearchContextProvider>
           <SearchBar />
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import { NavBarHome } from "../components/NavBarHome"
-
+import Image from "next/image";
 
 export default function HomePage() {
 	
 	return (
 		<>
 			<NavBarHome/>
+			<Image
+          src="/../images/foodbanklogo2.svg"
+          alt="logo-image"
+          width="300"
+          height="200"
+          className='flex justify-center'
+        />
 		</>
 	)
 }
