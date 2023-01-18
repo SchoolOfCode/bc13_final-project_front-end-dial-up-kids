aff3bb31dc6a78799ad9c4a584b3953133d8bb71
BenPuusta <benpuusta@gmail.com>
2023-01-18 16:36:06 +0000

    css from other branch

--- pages/find/index.tsx
+++ pages/find/index.tsx
@@ -8,7 +8,6 @@ import Link from "next/link";
 import Map from "../../components/map";
 import { NavBar } from "../../components/NavBar/NavBar";
 import { useRouter } from "next/navigation";
-import supabase from "../../components/supabaseClient";
 //import { Navbar } from "@nextui-org/react"; for later checking
 
 export default function About() {
@@ -23,26 +22,13 @@ export default function About() {
     setLocation,
     bank,
     setBank,
-    comments,
-    setComments,
   ]: any = useSearchContext();
   const router = useRouter();
-
-  async function fetchComments(index: number) {
-    let slugData = searchResults[index].foodbank.slug;
-    const { data, error } = await supabase
-      .from("comments")
-      .select()
-      .like("slug", slugData);
-	  console.log("supabase url", supabase)
-    setComments(data);
-  }
 
   function moreInfo(index: number) {
     setBank(searchResults[index]);
     setLocation(searchResults[index].lat_lng);
     router.push("/moreInfoBank");
-    fetchComments(index);
   }
 
   function handleCard(index: number) {
@@ -51,47 +37,22 @@ export default function About() {
   }
 
   return (
-    <div id='everything'>
+    <div id="everything">
+      <div className="p-3">
       <NavBar />
       <SearchBar />
-      <div id='mobile-content' className="md:flex-col">
-        <input></input>
-        <p>toggle bar</p> 
-        <Map coord={location}  />
-        <div id="List" className="
+      </div>
+      <div id="mobile-content" className=" lg:hidden md:flex-col">
+        <p>toggle bar</p>
+        <Map coord={location} />
+        <div
+          id="List"
+          className="
           min-w-[33%] max-w-[33%] overflow-auto
           ">
-            {searchResults.map((element: any, index: number) => {
-              return (
-                <Card 
-                  isPressable
-                  key={uuidv4()}
-                  onPress={() => {
-                    handleCard(index);
-                  }}>
-                  <p>{element.name}</p>
-                  <p>{element.address}</p>
-                  <div>
-                    <button
-                      onClick={() => {
-                        moreInfo(index);
-                      }}>
-                      More Info
-                    </button>
-                  </div>
-                </Card>
-              );
-            })}
-        </div>
-      </div>
-      <div id="desktop-content" className="flex flex-row justify-items-start bg-red-500 min-h-[60vh] max-h-[72vh] ">
-        <Map coord={location}  />
-        <div id="List" className="
-        min-w-[33%] max-w-[33%] overflow-auto
-        ">
           {searchResults.map((element: any, index: number) => {
             return (
-              <Card 
+              <Card
                 isPressable
                 key={uuidv4()}
                 onPress={() => {
@@ -110,6 +71,44 @@ export default function About() {
               </Card>
             );
           })}
+        </div>
+      </div>
+      <div
+        id="desktop-content"
+        className=" flex flex-row justify-items-start bg-red-500 min-h-[80vh] max-h-[72vh] ">
+        <Map coord={location} />
+        <div className="
+        min-w-[33%] max-w-[33%] overflow-auto my-3 pr-3">
+          <div
+          id="List"
+          className="
+          
+        ">
+          {searchResults.map((element: any, index: number) => {
+            return (
+             
+                <Card
+                isPressable
+                key={uuidv4()}
+                onPress={() => {
+                  handleCard(index);
+                }}>
+                <p>{element.name}</p>
+                <p>{element.address}</p>
+                <div>
+                  <button
+                    onClick={() => {
+                      moreInfo(index);
+                    }}>
+                    More Info
+                  </button>
+                </div>
+              </Card>
+               
+            );
+          })}
+         
+         </div>
         </div>
       </div>
     </div>
