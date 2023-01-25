1a6fa3afeba853b975747a289944f8c9e6abe412
BenPuusta <benpuusta@gmail.com>
2023-01-25 18:26:10 +0000

    another card highlight

--- components/FindList.tsx
+++ components/FindList.tsx
@@ -1,4 +1,4 @@
-const key = process.env.NEXT_PUBLIC_GOOGLE_KEY
+const key = process.env.NEXT_PUBLIC_GOOGLE_KEY;
 import { useEffect, useState } from "react";
 
 // import { Card, Button } from "flowbite-react";
@@ -8,10 +8,18 @@ import supabase from "./supabaseClient";
 import FoodBankCard from "./FoodBankCard/FoodBankCard";
 
 export default function FindList() {
-	const { searchResults, cardIndex, setCardIndex, setBank, setLocation, setComments, setMapCode }: any = useSearchContext();
-	const router = useRouter();
+  const {
+    searchResults,
+    cardIndex,
+    setCardIndex,
+    setBank,
+    setLocation,
+    setComments,
+    setMapCode,
+  }: any = useSearchContext();
+  const router = useRouter();
 
-	async function fetchComments(index: number) {
+  async function fetchComments(index: number) {
     let slugData = searchResults[index].foodbank.slug;
     const { data, error } = await supabase
       .from("comments")
@@ -21,11 +29,13 @@ export default function FindList() {
     setComments(data);
   }
 
-  async function updateMap(key:any, location:any ) {
-    setMapCode(`https://www.google.com/maps/embed/v1/place?key=${key}&q=${location}` ) 
+  async function updateMap(key: any, location: any) {
+    setMapCode(
+      `https://www.google.com/maps/embed/v1/place?key=${key}&q=${location}`
+    );
   }
 
-	function moreInfo(index: number) {
+  function moreInfo(index: number) {
     setBank(searchResults[index]);
     setLocation(searchResults[index].lat_lng);
     router.push("/moreInfoBank");
@@ -36,76 +46,24 @@ export default function FindList() {
     setLocation(searchResults[index].lat_lng);
     console.log(`handleCard: `, location);
     updateMap(key, searchResults[index].lat_lng);
+    setCardIndex(index);
   }
 
-  const [cssToggle, setCssToggle] = useState(``)  
-
-
-  const cardSelectColour = `
-  bg-lime-400 dark:bg-green-400
-  `;
-
-  useEffect(()=>{
-	setCssToggle(``)
-    setCssToggle (cardSelectColour)
-  },[cardIndex])
-  
-    function selectedCardCheck(index:number) {
-      if (cardIndex === index)
-        return cssToggle
-    }
-  
-
-
-
-	return <div
-		className="space-y-1">
-		{searchResults.map((element: any, index: number) => { 
-
-			return (
-				<FoodBankCard element={element} key={index} index={index} handleCard={()=>handleCard(index)} moreInfo={()=>moreInfo(index)}/>
-				// <Card 
-				// 	key={index}
-				// 	data-testid={`card-${index}`}
-				// 	onClick={() => {
-				// 		handleCard(index);
-				// 		setCardIndex(index);
-				// 		selectedCardCheck(index)
-				// 	}}
-				// 	className={ `hover:cursor-pointer ${selectedCardCheck(index)}
-					
-				// 	${cssDarkMode} ${cssLightMode}
-				// 	${selectedCardCheck(index)}
-				// 	`}
-	
-				// >
-				// 	<h5 className="text-l font-bold tracking-tight text-gray-900  dark:text-white">
-				// 		{element.name}
-				// 	</h5>
-				// 	<p className="font-light text-gray-900 dark:text-gray-300">
-				// 		{element.address}
-				// 	</p>
-				// 	<Button
-				// 		onClick={() => {
-				// 			moreInfo(index);
-				// 		}}
-				// 	>
-				// 		More Info
-				// 		<svg
-				// 			className="ml-2 -mr-1 h-4 w-4"
-				// 			fill="currentColor"
-				// 			viewBox="0 0 20 20"
-				// 			xmlns="http://www.w3.org/2000/svg"
-				// 		>
-				// 			<path
-				// 				fillRule="evenodd"
-				// 				d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-				// 				clipRule="evenodd"
-				// 			/>
-				// 		</svg>
-				// 	</Button>
-				// </Card>
-			);
-		})}
-	</div>
+  return (
+    <div className="space-y-1">
+      {searchResults.map((element: any, index: number) => {
+        return (
+          <FoodBankCard
+            element={element}
+			cardIndex={cardIndex}
+            key={index}
+            index={index}
+            handleCard={() => handleCard(index)}
+            moreInfo={() => moreInfo(index)}
+          />
+          
+        );
+      })}
+    </div>
+  );
 }
--- components/FoodBankCard/FoodBankCard.tsx
+++ components/FoodBankCard/FoodBankCard.tsx
@@ -1,62 +1,59 @@
 import React from "react";
-const key = process.env.NEXT_PUBLIC_GOOGLE_KEY
+const key = process.env.NEXT_PUBLIC_GOOGLE_KEY;
 import { useEffect, useState } from "react";
 import { Card, Button } from "flowbite-react";
 
 type Props = {};
 
-export default function FoodBankCard ({handleCard, element, moreInfo}:any) {
-
-    const cssLightMode =`
+export default function FoodBankCard({
+  handleCard,
+  element,
+  moreInfo,
+  cardIndex,
+  index
+}: any) {
+  const cssLightMode = `
     hover:bg-blue-400 
     active:bg-blue-500
   `;
-  
+
   const cssDarkMode = `
     dark:hover:bg-blue-800
     dark:active:bg-blue-900
     `;
 
-  const [toggle, setToggle] = useState(false)
-
-  return <Card 
-
-  onClick={() => {
-      handleCard();
-      setToggle(!toggle)
-      setCssToggle(false)
-  }}
-  className={ `hover:cursor-pointer}
-  
-  ${cssDarkMode} ${cssLightMode}
-   ${toggle?`bg-green-400`: ''}
-  `}
-
->
-  <h5 className="text-l font-bold tracking-tight text-gray-900  dark:text-white">
-      {element.name}
-  </h5>
-  <p className="font-light text-gray-900 dark:text-gray-300">
-      {element.address}
-  </p>
-  <Button
-      onClick={() => {
+  return (
+    <Card
+      onClick= {handleCard}
+      className={`
+      hover:cursor-pointer
+      ${cssDarkMode} 
+      ${cssLightMode}
+      ${cardIndex === index ? `bg-green-400` : ""}
+              `}>
+      <h5 className="text-l font-bold tracking-tight text-gray-900  dark:text-white">
+        {element.name}
+      </h5>
+      <p className="font-light text-gray-900 dark:text-gray-300">
+        {element.address}
+      </p>
+      <Button
+        onClick={() => {
           moreInfo();
-      }}
-  >
-      More Info
-      <svg
+        }}>
+        More Info
+        <svg
           className="ml-2 -mr-1 h-4 w-4"
           fill="currentColor"
           viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-      >
+          xmlns="http://www.w3.org/2000/svg">
           <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
+            fillRule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
           />
-      </svg>
-  </Button>
-</Card>
-};
+        </svg>
+      </Button>
+    </Card>
+  );
+}
